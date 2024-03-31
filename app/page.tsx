75bc0446bb999a05ba8146bbc7211c3292fc2d94
KATT <alexander@n1s.se>
2024-03-31 21:42:22 +0200

    add `useFormState()` form

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,29 @@
 "use client";
 
+import { useFormState } from "react-dom";
 import { myAction } from "./_action";
 
 export default function Page() {
+  const [_, formStateDispatch] = useFormState(myAction, null);
   return (
-    <form action={myAction}>
-      <input type="submit" />
-    </form>
+    <>
+      <div>
+        <h2>
+          <code>form action=myAction</code>
+        </h2>
+        <form action={myAction}>
+          <input type="submit" />
+        </form>
+      </div>
+      <div>
+        <h2>
+          using <code>useFormState()</code>
+        </h2>
+
+        <form action={formStateDispatch}>
+          <input type="submit" />
+        </form>
+      </div>
+    </>
   );
 }
